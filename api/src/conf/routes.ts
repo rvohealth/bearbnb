6a71115bc7ec8bedd1334c39d6dee597aaa31da9
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-03-15 13:55:31 -0500

    Generate Place resource
    ```
    yarn psy g:resource v1/host/places Place name:citext style:enum:place_styles:cottage,cabin,lean_to,treehouse,tent,cave,dump sleeps:integer deleted_at:datetime
    ```

--- api/src/conf/routes.ts
+++ api/src/conf/routes.ts
@@ -1,5 +1,11 @@
 import { PsychicRouter } from '@rvoh/psychic'
 
 export default (r: PsychicRouter) => {
+  r.namespace('v1', r => {
+    r.namespace('host', r => {
+      r.resources('places')
+    })
+  })
+
   // add routes here, perhaps by running `yarn psy g:resource v1/pets Pet name:citext birthdate:date species:enum:pet_species:dog,cat,fish`
 }

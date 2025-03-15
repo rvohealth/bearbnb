61b45ffac324a6593f51f39640e940db599feea3
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-03-15 14:37:42 -0500

    Generate LocalizedText resource ``` yarn psy g:resource v1/host/localized-texts LocalizedText localizable_type:enum:localized_types:Host,Place,Room localizable_id:bigint locale:enum:locales:en-US,es-ES title:string markdown:text deleted_at:datetime ```

---
+++ api/src/app/models/LocalizedText.ts
@@ -0,0 +1,27 @@
+import { Decorators, DreamColumn, DreamSerializers } from '@rvoh/dream'
+import ApplicationModel from './ApplicationModel.js'
+
+const Deco = new Decorators<InstanceType<typeof LocalizedText>>()
+
+export default class LocalizedText extends ApplicationModel {
+  public get table() {
+    return 'localized_texts' as const
+  }
+
+  public get serializers(): DreamSerializers<LocalizedText> {
+    return {
+      default: 'LocalizedTextSerializer',
+      summary: 'LocalizedTextSummarySerializer',
+    }
+  }
+
+  public id: DreamColumn<LocalizedText, 'id'>
+  public localizableType: DreamColumn<LocalizedText, 'localizableType'>
+  public localizableId: DreamColumn<LocalizedText, 'localizableId'>
+  public locale: DreamColumn<LocalizedText, 'locale'>
+  public title: DreamColumn<LocalizedText, 'title'>
+  public markdown: DreamColumn<LocalizedText, 'markdown'>
+  public deletedAt: DreamColumn<LocalizedText, 'deletedAt'>
+  public createdAt: DreamColumn<LocalizedText, 'createdAt'>
+  public updatedAt: DreamColumn<LocalizedText, 'updatedAt'>
+}
---
+++ api/src/app/serializers/LocalizedTextSerializer.ts
@@ -0,0 +1,33 @@
+import { Attribute, DreamColumn, DreamSerializer } from '@rvoh/dream'
+import LocalizedText from '../models/LocalizedText.js'
+
+export class LocalizedTextSummarySerializer<
+  DataType extends LocalizedText,
+  Passthrough extends object,
+> extends DreamSerializer<DataType, Passthrough> {
+  @Attribute(LocalizedText)
+  public id: DreamColumn<LocalizedText, 'id'>
+}
+
+export default class LocalizedTextSerializer<
+  DataType extends LocalizedText,
+  Passthrough extends object,
+> extends LocalizedTextSummarySerializer<DataType, Passthrough> {
+  @Attribute(LocalizedText)
+  public localizableType: DreamColumn<LocalizedText, 'localizableType'>
+
+    @Attribute(LocalizedText)
+  public localizableId: DreamColumn<LocalizedText, 'localizableId'>
+
+    @Attribute(LocalizedText)
+  public locale: DreamColumn<LocalizedText, 'locale'>
+
+    @Attribute(LocalizedText)
+  public title: DreamColumn<LocalizedText, 'title'>
+
+    @Attribute(LocalizedText)
+  public markdown: DreamColumn<LocalizedText, 'markdown'>
+
+    @Attribute(LocalizedText)
+  public deletedAt: DreamColumn<LocalizedText, 'deletedAt'>
+}

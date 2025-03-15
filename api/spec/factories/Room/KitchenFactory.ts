56369442d011e6e6fbcfa30e987f3a70983c438c
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-03-15 14:41:25 -0500

    Update KitchenFactory with array appliances

--- api/spec/factories/Room/KitchenFactory.ts
+++ api/spec/factories/Room/KitchenFactory.ts
@@ -1,9 +1,9 @@
 import { UpdateableProperties } from '@rvoh/dream'
 import Kitchen from '../../../src/app/models/Room/Kitchen.js'
-
-let counter = 0
+import createPlace from '../PlaceFactory.js'
 
 export default async function createRoomKitchen(attrs: UpdateableProperties<Kitchen> = {}) {
-  attrs.appliances ||= 'stove'
+  attrs.appliances ||= ['stove']
+  attrs.place ||= await createPlace()
   return await Kitchen.create(attrs)
 }

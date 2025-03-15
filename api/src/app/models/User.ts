348e571de4c50967384a0294d145ac6575a80e4c
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-03-15 12:40:36 -0500

    Generate User model ``` yarn psy g:model User email:citext first_name:string last_name:string --no-serializer ```

---
+++ api/src/app/models/User.ts
@@ -0,0 +1,17 @@
+import { Decorators, DreamColumn } from '@rvoh/dream'
+import ApplicationModel from './ApplicationModel.js'
+
+const Deco = new Decorators<InstanceType<typeof User>>()
+
+export default class User extends ApplicationModel {
+  public get table() {
+    return 'users' as const
+  }
+
+  public id: DreamColumn<User, 'id'>
+  public email: DreamColumn<User, 'email'>
+  public firstName: DreamColumn<User, 'firstName'>
+  public lastName: DreamColumn<User, 'lastName'>
+  public createdAt: DreamColumn<User, 'createdAt'>
+  public updatedAt: DreamColumn<User, 'updatedAt'>
+}

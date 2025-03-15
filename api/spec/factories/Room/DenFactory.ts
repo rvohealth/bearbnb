f9b140f6b374792651e57a5fe355dee11da2a1e8
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-03-15 14:42:17 -0500

    Add place to remaining STI room factories

--- api/spec/factories/Room/DenFactory.ts
+++ api/spec/factories/Room/DenFactory.ts
@@ -1,6 +1,8 @@
 import { UpdateableProperties } from '@rvoh/dream'
 import Den from '../../../src/app/models/Room/Den.js'
+import createPlace from '../PlaceFactory.js'
 
 export default async function createRoomDen(attrs: UpdateableProperties<Den> = {}) {
+  attrs.place ||= await createPlace()
   return await Den.create(attrs)
 }
--- api/spec/factories/Room/LivingRoomFactory.ts
+++ api/spec/factories/Room/LivingRoomFactory.ts
@@ -1,6 +1,8 @@
 import { UpdateableProperties } from '@rvoh/dream'
 import LivingRoom from '../../../src/app/models/Room/LivingRoom.js'
+import createPlace from '../PlaceFactory.js'
 
 export default async function createRoomLivingRoom(attrs: UpdateableProperties<LivingRoom> = {}) {
+  attrs.place ||= await createPlace()
   return await LivingRoom.create(attrs)
 }

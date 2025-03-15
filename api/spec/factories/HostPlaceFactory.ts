1c971187f1fb32adac9e431763df8640d2d002d0
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-03-15 13:58:08 -0500

    Generate HostPlace model ``` yarn psy g:model --no-serializer HostPlace Host:belongs_to Place:belongs_to deleted_at:datetime ```

---
+++ api/spec/factories/HostPlaceFactory.ts
@@ -0,0 +1,10 @@
+import { UpdateableProperties } from '@rvoh/dream'
+import HostPlace from '../../src/app/models/HostPlace.js'
+import createHost from './HostFactory.js'
+import createPlace from './PlaceFactory.js'
+
+export default async function createHostPlace(attrs: UpdateableProperties<HostPlace> = {}) {
+  attrs.host ||= await createHost()
+  attrs.place ||= await createPlace()
+  return await HostPlace.create(attrs)
+}

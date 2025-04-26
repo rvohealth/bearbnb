a9f690e916214a1d60bee52fb655b5e19176285e
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-04-26 13:47:48 -0500

    Generate LocalizedText resource
    ```console
    yarn psy g:resource v1/host/localized-texts LocalizedText localizable_type:enum:localized_types:Host,Place,Room localizable_id:bigint locale:enum:locales:en-US,es-ES title:string markdown:text deleted_at:datetime
    ```

---
+++ api/spec/factories/LocalizedTextFactory.ts
@@ -0,0 +1,11 @@
+import { UpdateableProperties } from '@rvoh/dream'
+import LocalizedText from '../../src/app/models/LocalizedText.js'
+
+let counter = 0
+
+export default async function createLocalizedText(attrs: UpdateableProperties<LocalizedText> = {}) {
+  attrs.locale ??= 'en-US'
+  attrs.title ??= `LocalizedText title ${++counter}`
+  attrs.markdown ??= `LocalizedText markdown ${counter}`
+  return await LocalizedText.create(attrs)
+}

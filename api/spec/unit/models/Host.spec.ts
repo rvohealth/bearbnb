91702b766d4776032cf73c5863db81571e228c41
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-04-26 13:02:13 -0500

    Host hasMany places through HostPlace Place hasMany hosts through HostPlace Association types synced Model specs
    To run migration:
    
    ```console
    yarn psy db:migrate
    ```
    
    If you changed a migration that you already ran, either:
    
    ```console
    yarn psy db:rollback
    yarn psy db:migrate
    ```
    or
    ```console
    yarn psy db:reset
    ```
    
    After adding HasMany/HasOne/BelongsTo associations:
    
    ```console
    yarn psy sync
    // yarn psy db:migrate will also sync
    ```
    
    To run model unit specs:
    
    ```console
    yarn uspec spec/unit/models
    ```

--- api/spec/unit/models/Host.spec.ts
+++ api/spec/unit/models/Host.spec.ts
@@ -1,3 +1,13 @@
+import createHost from '../../factories/HostFactory.js'
+import createHostPlace from '../../factories/HostPlaceFactory.js'
+import createPlace from '../../factories/PlaceFactory.js'
+
 describe('Host', () => {
-  it.todo('add a test here to get started building Host')
+  it('has many Places (through hostPlaces)', async () => {
+    const host = await createHost()
+    const place = await createPlace()
+    await createHostPlace({ host, place })
+
+    expect(await host.associationQuery('places').all()).toMatchDreamModels([place])
+  })
 })

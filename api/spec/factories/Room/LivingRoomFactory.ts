44271633c33d91cec7897278cd997a9753d05bf8
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-04-26 13:42:07 -0500

    Generate Room/LivingRoom STI model
    ```console
    yarn psy g:sti-child Room/LivingRoom extends Room
    ```

---
+++ api/spec/factories/Room/LivingRoomFactory.ts
@@ -0,0 +1,6 @@
+import { UpdateableProperties } from '@rvoh/dream'
+import RoomLivingRoom from '../../../src/app/models/Room/LivingRoom.js'
+
+export default async function createRoomLivingRoom(attrs: UpdateableProperties<RoomLivingRoom> = {}) {
+  return await RoomLivingRoom.create(attrs)
+}

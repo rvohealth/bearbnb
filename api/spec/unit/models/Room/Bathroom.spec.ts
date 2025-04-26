508a0d45692c5ccf3089232a6f202f10ce6b4a33
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-04-26 13:14:37 -0500

    Require bathOrShowerType to be set only on Bathroom Room models (achieved by adding a not-null constraint on `bath_or_shower_type` only when the type is `Bathroom`).
    Rename generated `RoomBathroom` to `Bathroom`.
    
    ```console
    yarn psy db:migrate
    yarn uspec spec/unit/models
    ```

--- api/spec/unit/models/Room/Bathroom.spec.ts
+++ api/spec/unit/models/Room/Bathroom.spec.ts
@@ -1,3 +1,19 @@
+import { DatabaseError } from 'pg'
+import Bathroom from '../../../../src/app/models/Room/Bathroom.js'
+import createPlace from '../../../factories/PlaceFactory.js'
+
 describe('Room/Bathroom', () => {
-  it.todo('add a test here to get started building Room/Bathroom')
+  it('requires bathOrShowerType to be set', async () => {
+    let error: DatabaseError
+    const place = await createPlace()
+
+    try {
+      await Bathroom.create({ place })
+    } catch (err) {
+      if (err instanceof DatabaseError) error = err
+      else throw err
+    }
+
+    expect(error!.message).toMatch('violates check constraint "rooms_not_null_bath_or_shower_type"')
+  })
 })

301b53771e914c79205e3e55839997334bcfa88f
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-04-26 12:44:38 -0500

    Generate Place resource
    ```console
    yarn psy g:resource v1/host/places Place name:citext style:enum:place_styles:cottage,cabin,lean_to,treehouse,tent,cave,dump sleeps:integer deleted_at:datetime
    ```

---
+++ api/spec/unit/models/Place.spec.ts
@@ -0,0 +1,3 @@
+describe('Place', () => {
+  it.todo('add a test here to get started building Place')
+})
---
+++ api/src/app/controllers/V1/Host/PlacesController.ts
@@ -0,0 +1,67 @@
+import { OpenAPI } from '@rvoh/psychic'
+import V1HostBaseController from './BaseController.js'
+import Place from '../../../models/Place.js'
+
+const openApiTags = ['places']
+
+export default class V1HostPlacesController extends V1HostBaseController {
+  @OpenAPI(Place, {
+    status: 201,
+    tags: openApiTags,
+    description: 'Create a Place',
+  })
+  public async create() {
+    //    const place = await this.currentUser.createAssociation('places', this.paramsFor(Place))
+    //    this.created(place)
+  }
+
+  @OpenAPI(Place, {
+    status: 200,
+    tags: openApiTags,
+    description: 'Fetch multiple Places',
+    many: true,
+    serializerKey: 'summary',
+  })
+  public async index() {
+    //    const places = await this.currentUser.associationQuery('places').all()
+    //    this.ok(places)
+  }
+
+  @OpenAPI(Place, {
+    status: 200,
+    tags: openApiTags,
+    description: 'Fetch a Place',
+  })
+  public async show() {
+    //    const place = await this.place()
+    //    this.ok(place)
+  }
+
+  @OpenAPI(Place, {
+    status: 204,
+    tags: openApiTags,
+    description: 'Update a Place',
+  })
+  public async update() {
+    //    const place = await this.place()
+    //    await place.update(this.paramsFor(Place))
+    //    this.noContent()
+  }
+
+  @OpenAPI({
+    status: 204,
+    tags: openApiTags,
+    description: 'Destroy a Place',
+  })
+  public async destroy() {
+    //    const place = await this.place()
+    //    await place.destroy()
+    //    this.noContent()
+  }
+
+  private async place() {
+    // return await this.currentUser.associationQuery('places').findOrFail(
+    //   this.castParam('id', 'string')
+    // )
+  }
+}

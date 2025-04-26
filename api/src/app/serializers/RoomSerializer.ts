1ccbaf0454ed18e236b6455922354d9aba4d45c7
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-04-26 13:06:26 -0500

    Generate Room resource
    ```console
    yarn psy g:resource v1/host/places/rooms Room type:enum:room_types:Bathroom,Bedroom,Kitchen,Den,LivingRoom,Garage Place:belongs_to position:integer deleted_at:datetime
    ```

---
+++ api/src/app/serializers/RoomSerializer.ts
@@ -0,0 +1,26 @@
+import { Attribute, DreamColumn, DreamSerializer } from '@rvoh/dream'
+import Room from '../models/Room.js'
+
+export class RoomSummarySerializer<
+  DataType extends Room,
+  Passthrough extends object,
+> extends DreamSerializer<DataType, Passthrough> {
+  @Attribute(Room)
+  public id: DreamColumn<Room, 'id'>
+}
+
+export default class RoomSerializer<
+  DataType extends Room,
+  Passthrough extends object,
+> extends RoomSummarySerializer<DataType, Passthrough> {
+  @Attribute(Room)
+  public type: DreamColumn<Room, 'type'>
+
+  
+
+    @Attribute(Room)
+  public position: DreamColumn<Room, 'position'>
+
+    @Attribute(Room)
+  public deletedAt: DreamColumn<Room, 'deletedAt'>
+}

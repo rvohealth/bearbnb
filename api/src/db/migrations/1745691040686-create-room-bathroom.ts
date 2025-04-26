fcddee16dd7bb28b28a5c1b561cd0a2ea126a32d
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-04-26 13:11:26 -0500

    Generate Room/Bathroom STI model
    ```console
    yarn psy g:sti-child --help
    yarn psy g:sti-child Room/Bathroom extends Room bath_or_shower_type:enum:bath_or_shower_types:bath,shower,bath_and_shower,none
    ```

---
+++ api/src/db/migrations/1745691040686-create-room-bathroom.ts
@@ -0,0 +1,29 @@
+import { Kysely, sql } from 'kysely'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createType('bath_or_shower_types_enum')
+    .asEnum([
+      'bath',
+      'shower',
+      'bath_and_shower',
+      'none'
+    ])
+    .execute()
+
+  await db.schema
+    .alterTable('rooms')
+    .addColumn('bath_or_shower_type', sql`bath_or_shower_types_enum`)
+    .execute()
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable('rooms')
+    .dropColumn('bath_or_shower_type')
+    .execute()
+
+  await db.schema.dropType('bath_or_shower_types_enum').execute()
+}

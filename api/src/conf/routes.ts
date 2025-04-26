637c694a7e60f1593471a3f8030b891c1c9a379d
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-04-26 13:46:21 -0500

    Nest rooms resource inside of places resource

--- api/src/conf/routes.ts
+++ api/src/conf/routes.ts
@@ -3,11 +3,9 @@ import { PsychicRouter } from '@rvoh/psychic'
 export default (r: PsychicRouter) => {
   r.namespace('v1', r => {
     r.namespace('host', r => {
-      r.namespace('places', r => {
+      r.resources('places', r => {
         r.resources('rooms')
       })
-
-      r.resources('places')
     })
   })
 }

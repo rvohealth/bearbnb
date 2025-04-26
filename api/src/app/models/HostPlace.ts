258c6487e57e4e1c761bc237ac22ec7db8bb102b
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-04-26 12:50:19 -0500

    Generate HostPlace model
    ```console
    yarn psy g:model --no-serializer HostPlace Host:belongs_to Place:belongs_to deleted_at:datetime
    ```

---
+++ api/src/app/models/HostPlace.ts
@@ -0,0 +1,25 @@
+import { Decorators, DreamColumn } from '@rvoh/dream'
+import ApplicationModel from './ApplicationModel.js'
+import Host from './Host.js'
+import Place from './Place.js'
+
+const deco = new Decorators<typeof HostPlace>()
+
+export default class HostPlace extends ApplicationModel {
+  public override get table() {
+    return 'host_places' as const
+  }
+
+  public id: DreamColumn<HostPlace, 'id'>
+  public deletedAt: DreamColumn<HostPlace, 'deletedAt'>
+  public createdAt: DreamColumn<HostPlace, 'createdAt'>
+  public updatedAt: DreamColumn<HostPlace, 'updatedAt'>
+
+  @deco.BelongsTo('Host')
+  public host: Host
+  public hostId: DreamColumn<HostPlace, 'hostId'>
+
+  @deco.BelongsTo('Place')
+  public place: Place
+  public placeId: DreamColumn<HostPlace, 'placeId'>
+}

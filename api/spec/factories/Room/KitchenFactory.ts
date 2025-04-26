0d0bced99747618f68d2205c3ccd7dd5db63b07d
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-04-26 13:58:04 -0500

    Set up polymorphic associations between LocalizedText and Host/Place/Room.
    Create a default LocalizedText for
    each Host/Place/Room.
    
    ```console
    yarn psy db:migrate
    yarn uspec spec/unit/models
    ```

--- api/spec/factories/Room/KitchenFactory.ts
+++ api/spec/factories/Room/KitchenFactory.ts
@@ -1,7 +1,9 @@
 import { UpdateableProperties } from '@rvoh/dream'
 import Kitchen from '../../../src/app/models/Room/Kitchen.js'
+import createPlace from '../PlaceFactory.js'
 
 export default async function createRoomKitchen(attrs: UpdateableProperties<Kitchen> = {}) {
+  attrs.place ??= await createPlace()
   attrs.appliances ??= ['stove']
   return await Kitchen.create(attrs)
 }

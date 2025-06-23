f0d8d156707f7529a54f2eeb4fcdaede39dc2c38
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-06-22 19:49:30 -0500

    Generate Place resource
    ```console
    yarn psy g:resource --owning-model=Host v1/host/places Place name:citext style:enum:place_styles:cottage,cabin,lean_to,treehouse,tent,cave,dump sleeps:integer deleted_at:datetime:optional
    ```

---
+++ api/src/app/controllers/V1/Host/PlacesController.ts
@@ -0,0 +1,70 @@
+import { OpenAPI } from '@rvoh/psychic'
+import Place from '../../../models/Place.js'
+import V1HostBaseController from './BaseController.js'
+
+const openApiTags = ['places']
+
+export default class V1HostPlacesController extends V1HostBaseController {
+  @OpenAPI(Place, {
+    status: 200,
+    tags: openApiTags,
+    description: 'Fetch multiple Places',
+    many: true,
+    serializerKey: 'summary',
+  })
+  public async index() {
+    // const places = await this.currentHost.associationQuery('places')
+    //   .preloadFor('summary')
+    //   .all()
+    // this.ok(places)
+  }
+
+  @OpenAPI(Place, {
+    status: 200,
+    tags: openApiTags,
+    description: 'Fetch a Place',
+  })
+  public async show() {
+    // const place = await this.place()
+    // this.ok(place)
+  }
+
+  @OpenAPI(Place, {
+    status: 201,
+    tags: openApiTags,
+    description: 'Create a Place',
+  })
+  public async create() {
+    // let place = await this.currentHost.createAssociation('places', this.paramsFor(Place))
+    // if (place.isPersisted) place = await place.loadFor('default').execute()
+    // this.created(place)
+  }
+
+  @OpenAPI(Place, {
+    status: 204,
+    tags: openApiTags,
+    description: 'Update a Place',
+  })
+  public async update() {
+    // const place = await this.place()
+    // await place.update(this.paramsFor(Place))
+    // this.noContent()
+  }
+
+  @OpenAPI({
+    status: 204,
+    tags: openApiTags,
+    description: 'Destroy a Place',
+  })
+  public async destroy() {
+    // const place = await this.place()
+    // await place.destroy()
+    // this.noContent()
+  }
+
+  private async place() {
+    // return await this.currentHost.associationQuery('places')
+    //   .preloadFor('default')
+    //   .findOrFail(this.castParam('id', 'string'))
+  }
+}
---
+++ api/src/db/migrations/1750639740738-create-place.ts
@@ -0,0 +1,38 @@
+import { DreamMigrationHelpers } from '@rvoh/dream'
+import { Kysely, sql } from 'kysely'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function up(db: Kysely<any>): Promise<void> {
+  await DreamMigrationHelpers.createExtension(db, 'citext')
+
+  await db.schema
+    .createType('place_styles_enum')
+    .asEnum([
+      'cottage',
+      'cabin',
+      'lean_to',
+      'treehouse',
+      'tent',
+      'cave',
+      'dump'
+    ])
+    .execute()
+
+  await db.schema
+    .createTable('places')
+    .addColumn('id', 'bigserial', col => col.primaryKey())
+    .addColumn('name', sql`citext`, col => col.notNull())
+    .addColumn('style', sql`place_styles_enum`, col => col.notNull())
+    .addColumn('sleeps', 'integer', col => col.notNull())
+    .addColumn('deleted_at', 'timestamp')
+    .addColumn('created_at', 'timestamp', col => col.notNull())
+    .addColumn('updated_at', 'timestamp', col => col.notNull())
+    .execute()
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('places').execute()
+
+  await db.schema.dropType('place_styles_enum').execute()
+}

f48bb1e8a3173dfaa9535fda48fb06225a1e9a12
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-06-22 21:19:29 -0500

    Rename generated RoomLivingRoom to LivingRoom
    ```console
    yarn psy db:migrate
    yarn uspec spec/unit/models
    ```

--- api/spec/factories/Room/LivingRoomFactory.ts
+++ api/spec/factories/Room/LivingRoomFactory.ts
@@ -1,8 +1,8 @@
 import { UpdateableProperties } from '@rvoh/dream'
-import RoomLivingRoom from '../../../src/app/models/Room/LivingRoom.js'
+import LivingRoom from '../../../src/app/models/Room/LivingRoom.js'
 
-export default async function createRoomLivingRoom(attrs: UpdateableProperties<RoomLivingRoom> = {}) {
-  return await RoomLivingRoom.create({
+export default async function createRoomLivingRoom(attrs: UpdateableProperties<LivingRoom> = {}) {
+  return await LivingRoom.create({
     ...attrs,
   })
 }

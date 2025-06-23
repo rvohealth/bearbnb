5beaa390543325e8577a50b10fa8443b01a6a0da
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-06-23 17:49:32 -0500

    Generate LocalizedText resource
    ```console
    yarn psy g:resource --only=update,destroy v1/host/localized-texts LocalizedText localizable_type:enum:localized_types:Host,Place,Room localizable_id:bigint locale:enum:locales:en-US,es-ES title:string markdown:text deleted_at:datetime:optional
    ```
    
    Leverages `--only=update,destroy` to only generate the show, update, and destroy actions. Each polymorphically associated model's controller will have an action for creating an associated LocalizedText model, and Host-facing serializer can display its LocalizedTexts, but the actions for maintaining all LocalizedText will be centralized in the LocalizedText controller.

---
+++ api/src/app/controllers/V1/Host/LocalizedTextsController.ts
@@ -0,0 +1,35 @@
+import { OpenAPI } from '@rvoh/psychic'
+import V1HostBaseController from './BaseController.js'
+import LocalizedText from '../../../models/LocalizedText.js'
+
+const openApiTags = ['localized-texts']
+
+export default class V1HostLocalizedTextsController extends V1HostBaseController {
+  @OpenAPI(LocalizedText, {
+    status: 204,
+    tags: openApiTags,
+    description: 'Update a LocalizedText',
+  })
+  public async update() {
+    // const localizedText = await this.localizedText()
+    // await localizedText.update(this.paramsFor(LocalizedText))
+    // this.noContent()
+  }
+
+  @OpenAPI({
+    status: 204,
+    tags: openApiTags,
+    description: 'Destroy a LocalizedText',
+  })
+  public async destroy() {
+    // const localizedText = await this.localizedText()
+    // await localizedText.destroy()
+    // this.noContent()
+  }
+
+  private async localizedText() {
+    // return await this.currentUser.associationQuery('localizedTexts')
+    //   .preloadFor('default')
+    //   .findOrFail(this.castParam('id', 'string'))
+  }
+}
--- api/src/conf/routes.ts
+++ api/src/conf/routes.ts
@@ -3,6 +3,8 @@ import { PsychicRouter } from '@rvoh/psychic'
 export default (r: PsychicRouter) => {
   r.namespace('v1', r => {
     r.namespace('host', r => {
+      r.resources('localized-texts', { only: ['update', 'destroy'] })
+
       r.resources('places', r => {
         r.resources('rooms')
       })

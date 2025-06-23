27f2ae83a3f75097faaddd9e42fdbb17e5cf597b
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-06-22 20:54:38 -0500

    Fix generated routes: nest rooms resource inside of places resource

--- api/src/conf/routes.ts
+++ api/src/conf/routes.ts
@@ -3,13 +3,9 @@ import { PsychicRouter } from '@rvoh/psychic'
 export default (r: PsychicRouter) => {
   r.namespace('v1', r => {
     r.namespace('host', r => {
-      r.namespace('places', r => {
+      r.resources('places', r => {
         r.resources('rooms')
       })
-
-      r.resources('places')
     })
   })
-
-  // add routes here, perhaps by running `yarn psy g:resource v1/pets Pet name:citext birthdate:date species:enum:pet_species:dog,cat,fish`
 }

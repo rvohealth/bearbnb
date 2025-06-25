2a58f8fd8d4e91383a502bad1fd2d18b4cf0f061
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-06-24 20:52:36 -0500

    Sortable
    ```console
    yarn uspec spec/unit/models/Room.spec.ts
    yarn uspec
    ```

---
+++ api/src/db/migrations/1750977354492-add-deferrable-unique-constraint-to-rooms.ts
@@ -0,0 +1,13 @@
+import { DreamMigrationHelpers } from '@rvoh/dream'
+import { Kysely } from 'kysely'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function up(db: Kysely<any>): Promise<void> {
+  await DreamMigrationHelpers.addDeferrableUniqueConstraint(db, 'room_position_contraint', {
+    table: 'rooms',
+    columns: ['place_id', 'position'],
+  })
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function down(db: Kysely<any>): Promise<void> {}

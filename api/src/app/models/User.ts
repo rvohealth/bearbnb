c2cae55b99eb3b77160bce651445c124fa3a8138
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-16 09:03:14 -0500

    Generate User model
    The User model is primaryily used for authentication
    and associating the authenticated user with resources.
    Since we haven't selected an authentication strategy
    yet, we'll simply start with an email address.
    
    citext ensures both that when we create a unique
    constraint, the email address will be case-insensitively
    unique, and that all queries are automatically case-
    insensitve.
    
    ```console
    yarn psy db:create
    yarn psy g:model --no-serializer User email:citext
    ```

---
+++ api/src/app/models/User.ts
@@ -0,0 +1,15 @@
+import { Decorators, DreamColumn } from '@rvoh/dream'
+import ApplicationModel from '@models/ApplicationModel.js'
+
+const deco = new Decorators<typeof User>()
+
+export default class User extends ApplicationModel {
+  public override get table() {
+    return 'users' as const
+  }
+
+  public id: DreamColumn<User, 'id'>
+  public email: DreamColumn<User, 'email'>
+  public createdAt: DreamColumn<User, 'createdAt'>
+  public updatedAt: DreamColumn<User, 'updatedAt'>
+}

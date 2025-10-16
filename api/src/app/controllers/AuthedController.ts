934c6d008a7deafeed3c68bc99ca3f2bcbc45ff6
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-16 16:55:01 -0500

    PlacesController specs passing
    ```console
    yarn uspec spec/unit/controllers/V1/Host/PlacesController.spec.ts
    ```

--- api/src/app/controllers/AuthedController.ts
+++ api/src/app/controllers/AuthedController.ts
@@ -1,30 +1,27 @@
 import AppEnv from '@conf/AppEnv.js'
 import ApplicationController from '@controllers/ApplicationController.js'
+import User from '@models/User.js'
 import { Encrypt } from '@rvoh/dream'
 import { BeforeAction } from '@rvoh/psychic'
-/** uncomment after creating User model */
-// import User from '@models/User.js'
 
 export default class AuthedController extends ApplicationController {
-  /** uncomment after creating User model */
-  // protected currentUser: User
+  protected currentUser: User
 
   @BeforeAction()
   protected async authenticate() {
-    /** uncomment after creating User model */
-    // const userId = this.authedUserId()
-    // if (!userId) return this.unauthorized()
-    //
-    // const user = await User.find(userId)
-    // if (!user) return this.unauthorized()
-    //
-    // this.currentUser = user
+    const userId = this.authedUserId()
+    if (!userId) return this.unauthorized()
+
+    const user = await User.find(userId)
+    if (!user) return this.unauthorized()
+
+    this.currentUser = user
   }
 
   protected authedUserId(): string | null {
     if (!AppEnv.isTest)
       throw new Error(
-        'The current authentication scheme is only for early development. Replace with a production grade authentication scheme.'
+        'The current authentication scheme is only for early development. Replace with a production grade authentication scheme.',
       )
 
     const token = (this.req.header('Authorization') ?? '').split(' ').at(-1)!

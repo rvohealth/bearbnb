6ee7dc9529835c61bd2eea612dbc1079cd174753
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-16 16:57:38 -0500

    Places index endpoint includes the name
    ```cosole
    yarn psy sync
    yarn uspec spec/unit/controllers/V1/Host/PlacesController.spec.ts
    ```

--- api/src/app/serializers/PlaceSerializer.ts
+++ api/src/app/serializers/PlaceSerializer.ts
@@ -1,13 +1,8 @@
-import { DreamSerializer } from '@rvoh/dream'
 import Place from '@models/Place.js'
+import { DreamSerializer } from '@rvoh/dream'
 
 export const PlaceSummarySerializer = (place: Place) =>
-  DreamSerializer(Place, place)
-    .attribute('id')
+  DreamSerializer(Place, place).attribute('id').attribute('name')
 
 export const PlaceSerializer = (place: Place) =>
-  PlaceSummarySerializer(place)
-    .attribute('name')
-    .attribute('style')
-    .attribute('sleeps')
-    .attribute('deletedAt')
+  PlaceSummarySerializer(place).attribute('style').attribute('sleeps').attribute('deletedAt')

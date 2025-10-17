1ab48c33b13bab6deae563f50793b2fb07201540
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-17 08:12:15 -0500

    Fix generated route: generator generated a namespace, but we want a nested resource

--- api/src/conf/routes.ts
+++ api/src/conf/routes.ts
@@ -4,14 +4,11 @@ import { PsychicRouter } from '@rvoh/psychic'
 export default function routes(r: PsychicRouter) {
   r.namespace('v1', r => {
     r.namespace('host', r => {
-      r.namespace('places', r => {
+      r.resources('places', r => {
         r.resources('rooms')
       })
-
-      r.resources('places')
     })
   })
 
   adminRoutes(r)
-  // add routes here, perhaps by running `yarn psy g:resource v1/pets Pet name:citext birthdate:date species:enum:pet_species:dog,cat,fish`
 }

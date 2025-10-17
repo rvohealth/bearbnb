ad5348945e67cdf8bd2e0ab6f8e09b3ad556ee83
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-17 08:10:39 -0500

    Generate Room resource
    ```console
    yarn psy g:resource --sti-base-serializer --owning-model=Place v1/host/places/rooms Room type:enum:room_types:Bathroom,Bedroom,Kitchen,Den,LivingRoom Place:belongs_to position:integer:optional deleted_at:datetime:optional
    ```

---
+++ api/spec/factories/RoomFactory.ts
@@ -0,0 +1,11 @@
+import { UpdateableProperties } from '@rvoh/dream'
+import Room from '@models/Room.js'
+import createPlace from '@spec/factories/PlaceFactory.js'
+
+export default async function createRoom(attrs: UpdateableProperties<Room> = {}) {
+  return await Room.create({
+    place: attrs.place ? null : await createPlace(),
+    type: 'Bathroom',
+    ...attrs,
+  })
+}

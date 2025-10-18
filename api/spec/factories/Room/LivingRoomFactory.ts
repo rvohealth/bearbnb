5ca8940a3e3d59a59a82f0f47942b33ab24d23ea
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-18 16:20:50 -0500

    Rename generated RoomLivingRoom to LivingRoom
    ```console
    yarn psy db:migrate
    ```

--- api/spec/factories/Room/LivingRoomFactory.ts
+++ api/spec/factories/Room/LivingRoomFactory.ts
@@ -1,8 +1,8 @@
+import LivingRoom from '@models/Room/LivingRoom.js'
 import { UpdateableProperties } from '@rvoh/dream'
-import RoomLivingRoom from '@models/Room/LivingRoom.js'
 
-export default async function createRoomLivingRoom(attrs: UpdateableProperties<RoomLivingRoom> = {}) {
-  return await RoomLivingRoom.create({
+export default async function createRoomLivingRoom(attrs: UpdateableProperties<LivingRoom> = {}) {
+  return await LivingRoom.create({
     ...attrs,
   })
 }
--- api/src/app/models/Room/LivingRoom.ts
+++ api/src/app/models/Room/LivingRoom.ts
@@ -1,15 +1,14 @@
-import { Decorators, DreamColumn, DreamSerializers, STI } from '@rvoh/dream'
 import Room from '@models/Room.js'
+import { Decorators, DreamSerializers, STI } from '@rvoh/dream'
 
-const deco = new Decorators<typeof RoomLivingRoom>()
+const deco = new Decorators<typeof LivingRoom>()
 
 @STI(Room)
-export default class RoomLivingRoom extends Room {
-  public override get serializers(): DreamSerializers<RoomLivingRoom> {
+export default class LivingRoom extends Room {
+  public override get serializers(): DreamSerializers<LivingRoom> {
     return {
       default: 'Room/LivingRoomSerializer',
       summary: 'Room/LivingRoomSummarySerializer',
     }
   }
-
 }

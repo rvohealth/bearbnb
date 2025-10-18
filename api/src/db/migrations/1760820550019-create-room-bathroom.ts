4dfed48cc9d94c9a518712cf0be8b905becd7a45
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-18 15:49:41 -0500

    Generate Room/Bathroom STI model
    ```console
    yarn psy g:sti-child --help
    yarn psy g:sti-child Room/Bathroom extends Room bath_or_shower_style:enum:bath_or_shower_styles:bath,shower,bath_and_shower,none
    ```

---
+++ api/src/db/migrations/1760820550019-create-room-bathroom.ts
@@ -0,0 +1,37 @@
+import { Kysely, sql } from 'kysely'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createType('bath_or_shower_styles_enum')
+    .asEnum([
+      'bath',
+      'shower',
+      'bath_and_shower',
+      'none'
+    ])
+    .execute()
+
+  await db.schema
+    .alterTable('rooms')
+    .addColumn('bath_or_shower_style', sql`bath_or_shower_styles_enum`)
+    .execute()
+
+  await db.schema
+    .alterTable('rooms')
+    .addCheckConstraint(
+      'rooms_not_null_bath_or_shower_style',
+      sql`type != 'RoomBathroom' OR bath_or_shower_style IS NOT NULL`,
+    )
+    .execute()
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable('rooms')
+    .dropColumn('bath_or_shower_style')
+    .execute()
+
+  await db.schema.dropType('bath_or_shower_types_enum').execute()
+}

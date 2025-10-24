4aaaf8b64e2e754e07f127fb0d0764ea6dc7f79c
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-24 16:29:25 -0400

    Generate HostPlace model
    ```console
    yarn psy g:model --no-serializer HostPlace Host:belongs_to Place:belongs_to deleted_at:datetime:optional
    ```

---
+++ api/spec/factories/HostPlaceFactory.ts
@@ -0,0 +1,12 @@
+import { UpdateableProperties } from '@rvoh/dream/types'
+import HostPlace from '@models/HostPlace.js'
+import createHost from '@spec/factories/HostFactory.js'
+import createPlace from '@spec/factories/PlaceFactory.js'
+
+export default async function createHostPlace(attrs: UpdateableProperties<HostPlace> = {}) {
+  return await HostPlace.create({
+    host: attrs.host ? null : await createHost(),
+    place: attrs.place ? null : await createPlace(),
+    ...attrs,
+  })
+}
---
+++ api/src/db/migrations/1761337753343-create-host-place.ts
@@ -0,0 +1,33 @@
+import { Kysely, sql } from 'kysely'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('host_places')
+    .addColumn('id', 'bigserial', col => col.primaryKey())
+    .addColumn('host_id', 'bigint', col => col.references('hosts.id').onDelete('restrict').notNull())
+    .addColumn('place_id', 'bigint', col => col.references('places.id').onDelete('restrict').notNull())
+    .addColumn('deleted_at', 'timestamp')
+    .addColumn('created_at', 'timestamp', col => col.notNull())
+    .addColumn('updated_at', 'timestamp', col => col.notNull())
+    .execute()
+
+  await db.schema
+    .createIndex('host_places_host_id')
+    .on('host_places')
+    .column('host_id')
+    .execute()
+
+  await db.schema
+    .createIndex('host_places_place_id')
+    .on('host_places')
+    .column('place_id')
+    .execute()
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropIndex('host_places_host_id').execute()
+  await db.schema.dropIndex('host_places_place_id').execute()
+  await db.schema.dropTable('host_places').execute()
+}

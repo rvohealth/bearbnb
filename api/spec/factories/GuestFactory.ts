cfae0605f2f905846ba5e6e4da003bf2e082a0ea
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-24 16:23:50 -0400

    Generate Guest model
    ```console
    yarn psy g:model Guest User:belongs_to
    ```

---
+++ api/spec/factories/GuestFactory.ts
@@ -0,0 +1,10 @@
+import { UpdateableProperties } from '@rvoh/dream/types'
+import Guest from '@models/Guest.js'
+import createUser from '@spec/factories/UserFactory.js'
+
+export default async function createGuest(attrs: UpdateableProperties<Guest> = {}) {
+  return await Guest.create({
+    user: attrs.user ? null : await createUser(),
+    ...attrs,
+  })
+}

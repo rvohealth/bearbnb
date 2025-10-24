0d0505b554a7cfd4fc072c7e4cc875babe98c259
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-24 16:26:12 -0400

    Generate Host model
    ```console
    yarn psy g:model Host User:belongs_to
    ```

---
+++ api/src/db/migrations/1761337565490-create-host.ts
@@ -0,0 +1,24 @@
+import { Kysely, sql } from 'kysely'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('hosts')
+    .addColumn('id', 'bigserial', col => col.primaryKey())
+    .addColumn('user_id', 'bigint', col => col.references('users.id').onDelete('restrict').notNull())
+    .addColumn('created_at', 'timestamp', col => col.notNull())
+    .addColumn('updated_at', 'timestamp', col => col.notNull())
+    .execute()
+
+  await db.schema
+    .createIndex('hosts_user_id')
+    .on('hosts')
+    .column('user_id')
+    .execute()
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropIndex('hosts_user_id').execute()
+  await db.schema.dropTable('hosts').execute()
+}

a0bc56db3e2d830847e523f1471702e8e41bc86a
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-24 16:35:11 -0400

    PlacesController specs passing
    ```console
    yarn uspec spec/unit/controllers/V1/Host/PlacesController.spec.ts
    // if it hangs, it's likely that you haven't implemented the controller
    // changes; simply adding `this.ok()` or `this.noContent()` to each action
    // in the controller will get the spec run to complete
    ```

--- api/src/app/controllers/V1/Host/BaseController.ts
+++ api/src/app/controllers/V1/Host/BaseController.ts
@@ -1,5 +1,15 @@
+import Host from '@models/Host.js'
+import { BeforeAction } from '@rvoh/psychic'
 import V1BaseController from '../BaseController.js'
 
 export default class V1HostBaseController extends V1BaseController {
+  protected currentHost: Host
 
+  @BeforeAction()
+  protected async loadCurrentHost() {
+    const host = await this.currentUser.associationQuery('host').first()
+    if (!host) return this.forbidden()
+
+    this.currentHost = host
+  }
 }

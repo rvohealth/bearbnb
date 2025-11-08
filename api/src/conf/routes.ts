7fbe764b9c18a31acc8139935bb3d037de941f80
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-11-08 10:52:15 -0600

    Generate Place resource
    ```console
    yarn psy g:resource --owning-model=Host v1/host/places Place name:citext style:enum:place_styles:cottage,cabin,lean_to,treehouse,tent,cave,dump sleeps:integer deleted_at:datetime:optional
    ```

--- api/src/conf/routes.ts
+++ api/src/conf/routes.ts
@@ -2,6 +2,12 @@ import adminRoutes from '@conf/routes.admin.js'
 import { PsychicRouter } from '@rvoh/psychic'
 
 export default function routes(r: PsychicRouter) {
+  r.namespace('v1', r => {
+    r.namespace('host', r => {
+      r.resources('places')
+    })
+  })
+
   adminRoutes(r)
   // add routes here, perhaps by running `yarn psy g:resource v1/pets Pet name:citext birthdate:date species:enum:pet_species:dog,cat,fish`
 }

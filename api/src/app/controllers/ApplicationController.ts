2f92295981cb5f171a7b41caa26cece5377b475d
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-11-08 12:46:41 -0600

    Places controller for Guests, including localization
    If you get ambiguous Typescript errors along the way, restart
    the ESLint server in your editor, and if that doesn't work,
    quit and re-open your editor
    
    ```console
    yarn psy g:controller V1/Guest/Places index show
    
    yarn psy sync --ignore-errors
    yarn psy sync
    
    yarn build
    yarn build:spec
    
    yarn uspec spec/unit/controllers/V1/Guest/PlacesController.spec.ts
    yarn uspec
    ```

--- api/src/app/controllers/ApplicationController.ts
+++ api/src/app/controllers/ApplicationController.ts
@@ -1,6 +1,7 @@
-import { PsychicController } from '@rvoh/psychic'
+import { BeforeAction, PsychicController } from '@rvoh/psychic'
 import { PsychicOpenapiNames } from '@rvoh/psychic/openapi'
 import psychicTypes from '@src/types/psychic.js'
+import { supportedLocales } from '@src/utils/i18n.js'
 
 export default class ApplicationController extends PsychicController {
   public override get psychicTypes() {
@@ -9,5 +10,18 @@ export default class ApplicationController extends PsychicController {
 
   public static override get openapiNames(): PsychicOpenapiNames<ApplicationController> {
     return ['default', 'mobile', 'validation']
+  }
+
+  @BeforeAction()
+  public configureSerializers() {
+    this.serializerPassthrough({
+      locale: this.locale,
+    })
+  }
+
+  protected get locale() {
+    const locale = this.headers['content-language']
+    const locales = supportedLocales()
+    return locales.includes(locale as (typeof locales)[number]) ? locale : 'en-US'
   }
 }

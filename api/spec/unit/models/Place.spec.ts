965b1bd089107e26101b5be90109bc64abfb30c9
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-11-08 13:16:23 -0600

    SoftDelete
    ```console
    yarn psy sync
    yarn uspec spec/unit/models/Place.spec.ts
    yarn uspec
    ```

--- api/spec/unit/models/Place.spec.ts
+++ api/spec/unit/models/Place.spec.ts
@@ -1,7 +1,12 @@
+import HostPlace from '@models/HostPlace.js'
+import LocalizedText from '@models/LocalizedText.js'
+import Place from '@models/Place.js'
+import Room from '@models/Room.js'
 import createHost from '@spec/factories/HostFactory.js'
 import createHostPlace from '@spec/factories/HostPlaceFactory.js'
 import createLocalizedText from '@spec/factories/LocalizedTextFactory.js'
 import createPlace from '@spec/factories/PlaceFactory.js'
+import createRoomKitchen from '@spec/factories/Room/KitchenFactory.js'
 
 describe('Place', () => {
   it('has many Hosts (through hostPlaces)', async () => {
@@ -36,5 +41,43 @@ describe('Place', () => {
     place = await place.passthrough({ locale: 'es-ES' }).load('currentLocalizedText').execute()
 
     expect(place.currentLocalizedText).toMatchDreamModel(esLocalizedText)
+  })
+
+  context('upon destruction', () => {
+    it('soft-deletes associated HostPlaces, Rooms, and LocalizedTexts', async () => {
+      const place = await createPlace()
+      const hostPlace = await createHostPlace({ place })
+      const room = await createRoomKitchen({ place })
+      const placeText = await place.associationQuery('localizedTexts').firstOrFail()
+      const roomText = await room.associationQuery('localizedTexts').firstOrFail()
+
+      expect(await Place.where({ id: place.id }).exists()).toBe(true)
+      expect(await HostPlace.where({ id: hostPlace.id }).exists()).toBe(true)
+      expect(await Room.where({ id: room.id }).exists()).toBe(true)
+      expect(await LocalizedText.where({ id: placeText.id }).exists()).toBe(true)
+      expect(await LocalizedText.where({ id: roomText.id }).exists()).toBe(true)
+
+      await place.destroy()
+
+      const placeQuery = Place.where({ id: place.id })
+      expect(await placeQuery.exists()).toBe(false)
+      expect(await placeQuery.removeAllDefaultScopes().exists()).toBe(true)
+
+      const hostPlaceQuery = HostPlace.where({ id: hostPlace.id })
+      expect(await hostPlaceQuery.exists()).toBe(false)
+      expect(await hostPlaceQuery.removeAllDefaultScopes().exists()).toBe(true)
+
+      const roomQuery = Room.where({ id: room.id })
+      expect(await roomQuery.exists()).toBe(false)
+      expect(await roomQuery.removeAllDefaultScopes().exists()).toBe(true)
+
+      const placeTextQuery = LocalizedText.where({ id: placeText.id })
+      expect(await placeTextQuery.exists()).toBe(false)
+      expect(await placeTextQuery.removeAllDefaultScopes().exists()).toBe(true)
+
+      const roomTextQuery = LocalizedText.where({ id: roomText.id })
+      expect(await roomTextQuery.exists()).toBe(false)
+      expect(await roomTextQuery.removeAllDefaultScopes().exists()).toBe(true)
+    })
   })
 })

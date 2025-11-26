3aaf0b3dbb08ac9d1694955ff47b0b8a95d870b8
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-11-26 13:18:32 -0600

    Rename generated RoomKitchen to Kitchen.
    No check constraint to update in the migration because, for
    array types, the generator always sets a default for array
    types (`{}` is the Postgres representation of an empty array),
    so even non-Kitchen Rooms will have non-null, empty-array `appliances`
    
    ```console
    yarn psy db:migrate
    ```

--- api/src/types/dream.ts
+++ api/src/types/dream.ts
@@ -59,6 +59,8 @@ us humans, he says:
 
 import { type CalendarDate, type DateTime } from '@rvoh/dream'
 import {
+  ApplianceTypesEnum,
+  ApplianceTypesEnumValues,
   BathOrShowerStylesEnum,
   BathOrShowerStylesEnumValues,
   BedTypesEnum,
@@ -389,8 +391,17 @@ export const schema = {
       default: ['dream:STI'],
       named: [],
     },
-    nonJsonColumnNames: ['bathOrShowerStyle', 'bedTypes', 'createdAt', 'deletedAt', 'id', 'placeId', 'position', 'type', 'updatedAt'],
+    nonJsonColumnNames: ['appliances', 'bathOrShowerStyle', 'bedTypes', 'createdAt', 'deletedAt', 'id', 'placeId', 'position', 'type', 'updatedAt'],
     columns: {
+      appliances: {
+        coercedType: {} as ApplianceTypesEnum[],
+        enumType: {} as ApplianceTypesEnum,
+        enumArrayType: [] as ApplianceTypesEnum[],
+        enumValues: ApplianceTypesEnumValues,
+        dbType: 'appliance_types_enum[]',
+        allowNull: false,
+        isArray: true,
+      },
       bathOrShowerStyle: {
         coercedType: {} as BathOrShowerStylesEnum | null,
         enumType: {} as BathOrShowerStylesEnum,
@@ -566,6 +577,7 @@ export const connectionTypeConfig = {
       'Place': 'places',
       'Room/Bathroom': 'rooms',
       'Room/Bedroom': 'rooms',
+      'Room/Kitchen': 'rooms',
       'Room': 'rooms',
       'User': 'users'
     },

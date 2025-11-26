b69550269ca8e21a1ebe72b3f848c1d1efd9ca9e
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-11-26 10:46:39 -0600

    Generate Guest model
    ```console
    yarn psy g:model Guest User:belongs_to
    ```

---
+++ api/src/db/migrations/1764175590862-create-guest.ts
@@ -0,0 +1,28 @@
+import { Kysely, sql } from 'kysely'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('guests')
+    .addColumn('id', 'uuid', col =>
+      col
+        .primaryKey()
+        .defaultTo(sql`uuid_generate_v4()`),
+    )
+    .addColumn('user_id', 'uuid', col => col.references('users.id').onDelete('restrict').notNull())
+    .addColumn('created_at', 'timestamp', col => col.notNull())
+    .addColumn('updated_at', 'timestamp', col => col.notNull())
+    .execute()
+
+  await db.schema
+    .createIndex('guests_user_id')
+    .on('guests')
+    .column('user_id')
+    .execute()
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropIndex('guests_user_id').execute()
+  await db.schema.dropTable('guests').execute()
+}

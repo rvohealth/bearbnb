555e10d286984e3a9c6e836f0d49969b0f206d90
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-11-26 10:45:34 -0600

    Generate User model
    The User model is primaryily used for authentication
    and associating the authenticated user with resources.
    Since we haven't selected an authentication strategy
    yet, we'll simply start with an email address.
    
    citext ensures both that when we create a unique
    constraint, the email address will be case-insensitively
    unique, and that all queries are automatically case-
    insensitve.
    
    ```console
    yarn psy db:create
    yarn psy g:model --no-serializer User email:citext
    ```

---
+++ api/src/db/migrations/1764175528349-create-user.ts
@@ -0,0 +1,24 @@
+import { DreamMigrationHelpers } from '@rvoh/dream/db'
+import { Kysely, sql } from 'kysely'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function up(db: Kysely<any>): Promise<void> {
+  await DreamMigrationHelpers.createExtension(db, 'citext')
+
+  await db.schema
+    .createTable('users')
+    .addColumn('id', 'uuid', col =>
+      col
+        .primaryKey()
+        .defaultTo(sql`uuid_generate_v4()`),
+    )
+    .addColumn('email', sql`citext`, col => col.notNull().unique())
+    .addColumn('created_at', 'timestamp', col => col.notNull())
+    .addColumn('updated_at', 'timestamp', col => col.notNull())
+    .execute()
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('users').execute()
+}

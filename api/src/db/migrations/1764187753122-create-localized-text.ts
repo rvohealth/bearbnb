618086587b0bbab4ab2840b91a817292e9e38a7c
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-11-26 14:09:23 -0600

    Generate LocalizedText resource
    ```console
    yarn psy g:resource --only=update,destroy v1/host/localized-texts LocalizedText localizable_type:enum:localized_types:Host,Place,Room localizable_id:uuid locale:enum:locales:en-US,es-ES title:string markdown:text deleted_at:datetime:optional
    ```
    
    Leverages `--only=update,destroy` to only generate the show, update, and destroy actions. Each polymorphically associated model's controller will have an action for creating an associated LocalizedText model, and Host-facing serializer can display its LocalizedTexts, but the actions for maintaining all LocalizedText will be centralized in the LocalizedText controller.

---
+++ api/src/db/migrations/1764187753122-create-localized-text.ts
@@ -0,0 +1,46 @@
+import { Kysely, sql } from 'kysely'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createType('localized_types_enum')
+    .asEnum([
+      'Host',
+      'Place',
+      'Room'
+    ])
+    .execute()
+
+  await db.schema
+    .createType('locales_enum')
+    .asEnum([
+      'en-US',
+      'es-ES'
+    ])
+    .execute()
+
+  await db.schema
+    .createTable('localized_texts')
+    .addColumn('id', 'uuid', col =>
+      col
+        .primaryKey()
+        .defaultTo(sql`uuid_generate_v4()`),
+    )
+    .addColumn('localizable_type', sql`localized_types_enum`, col => col.notNull())
+    .addColumn('localizable_id', 'uuid', col => col.notNull())
+    .addColumn('locale', sql`locales_enum`, col => col.notNull())
+    .addColumn('title', 'varchar(255)', col => col.notNull())
+    .addColumn('markdown', 'text', col => col.notNull())
+    .addColumn('deleted_at', 'timestamp')
+    .addColumn('created_at', 'timestamp', col => col.notNull())
+    .addColumn('updated_at', 'timestamp', col => col.notNull())
+    .execute()
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('localized_texts').execute()
+
+  await db.schema.dropType('localized_types_enum').execute()
+  await db.schema.dropType('locales_enum').execute()
+}

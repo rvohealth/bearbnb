6b04aa728eadd21c32f4bbdc02c7afc0ee4a47de
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-11-26 11:05:38 -0600

    Generate Place resource
    ```console
    yarn psy g:resource --owning-model=Host v1/host/places Place name:citext style:enum:place_styles:cottage,cabin,lean_to,treehouse,tent,cave,dump sleeps:integer deleted_at:datetime:optional
    ```

---
+++ api/src/db/migrations/1764176714209-create-place.ts
@@ -0,0 +1,42 @@
+import { DreamMigrationHelpers } from '@rvoh/dream/db'
+import { Kysely, sql } from 'kysely'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function up(db: Kysely<any>): Promise<void> {
+  await DreamMigrationHelpers.createExtension(db, 'citext')
+
+  await db.schema
+    .createType('place_styles_enum')
+    .asEnum([
+      'cottage',
+      'cabin',
+      'lean_to',
+      'treehouse',
+      'tent',
+      'cave',
+      'dump'
+    ])
+    .execute()
+
+  await db.schema
+    .createTable('places')
+    .addColumn('id', 'uuid', col =>
+      col
+        .primaryKey()
+        .defaultTo(sql`uuid_generate_v4()`),
+    )
+    .addColumn('name', sql`citext`, col => col.notNull())
+    .addColumn('style', sql`place_styles_enum`, col => col.notNull())
+    .addColumn('sleeps', 'integer', col => col.notNull())
+    .addColumn('deleted_at', 'timestamp')
+    .addColumn('created_at', 'timestamp', col => col.notNull())
+    .addColumn('updated_at', 'timestamp', col => col.notNull())
+    .execute()
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('places').execute()
+
+  await db.schema.dropType('place_styles_enum').execute()
+}

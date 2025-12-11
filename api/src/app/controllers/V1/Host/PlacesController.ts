0a2133a2620000326348034419803b4526584dc9
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-12-10 18:26:33 -0600

    PlacesController specs passing
    NOTE: if, after running `pnpm psy sync`, your editor starts
    showing errors like `Unsafe call of a(n) 'error' type typed value`,
    restart the ESLint server (the `ESLint: Restart ESLint Server`
    command in VSCode / Cursor)
    
    ```console
    pnpm uspec spec/unit/controllers/V1/Host/PlacesController.spec.ts
    // if it hangs, it's likely that you haven't implemented the controller
    // changes; simply adding `this.ok()` or `this.noContent()` to each action
    // in the controller will get the spec run to complete
    ```

--- api/src/app/controllers/V1/Host/PlacesController.ts
+++ api/src/app/controllers/V1/Host/PlacesController.ts
@@ -1,6 +1,8 @@
+import ApplicationModel from '@models/ApplicationModel.js'
+import HostPlace from '@models/HostPlace.js'
+import Place from '@models/Place.js'
 import { OpenAPI } from '@rvoh/psychic'
 import V1HostBaseController from './BaseController.js'
-import Place from '@models/Place.js'
 
 const openApiTags = ['places']
 
@@ -13,10 +15,11 @@ export default class V1HostPlacesController extends V1HostBaseController {
     serializerKey: 'summary',
   })
   public async index() {
-    // const places = await this.currentHost.associationQuery('places')
-    //   .preloadFor('summary')
-    //   .cursorPaginate({ cursor: this.castParam('cursor', 'string', { allowNull: true }) })
-    // this.ok(places)
+    const places = await this.currentHost
+      .associationQuery('places')
+      .preloadFor('summary')
+      .cursorPaginate({ cursor: this.castParam('cursor', 'string', { allowNull: true }) })
+    this.ok(places)
   }
 
   @OpenAPI(Place, {
@@ -25,8 +28,8 @@ export default class V1HostPlacesController extends V1HostBaseController {
     description: 'Fetch a Place',
   })
   public async show() {
-    // const place = await this.place()
-    // this.ok(place)
+    const place = await this.place()
+    this.ok(place)
   }
 
   @OpenAPI(Place, {
@@ -35,9 +38,14 @@ export default class V1HostPlacesController extends V1HostBaseController {
     description: 'Create a Place',
   })
   public async create() {
-    // let place = await this.currentHost.createAssociation('places', this.paramsFor(Place))
-    // if (place.isPersisted) place = await place.loadFor('default').execute()
-    // this.created(place)
+    let place = await ApplicationModel.transaction(async txn => {
+      const place = await Place.txn(txn).create(this.paramsFor(Place))
+      await HostPlace.txn(txn).create({ host: this.currentHost, place })
+      return place
+    })
+
+    if (place.isPersisted) place = await place.loadFor('default').execute()
+    this.created(place)
   }
 
   @OpenAPI(Place, {
@@ -46,9 +54,9 @@ export default class V1HostPlacesController extends V1HostBaseController {
     description: 'Update a Place',
   })
   public async update() {
-    // const place = await this.place()
-    // await place.update(this.paramsFor(Place))
-    // this.noContent()
+    const place = await this.place()
+    await place.update(this.paramsFor(Place))
+    this.noContent()
   }
 
   @OpenAPI({
@@ -57,14 +65,15 @@ export default class V1HostPlacesController extends V1HostBaseController {
     description: 'Destroy a Place',
   })
   public async destroy() {
-    // const place = await this.place()
-    // await place.destroy()
-    // this.noContent()
+    const place = await this.place()
+    await place.destroy()
+    this.noContent()
   }
 
   private async place() {
-    // return await this.currentHost.associationQuery('places')
-    //   .preloadFor('default')
-    //   .findOrFail(this.castParam('id', 'string'))
+    return await this.currentHost
+      .associationQuery('places')
+      .preloadFor('default')
+      .findOrFail(this.castParam('id', 'string'))
   }
 }

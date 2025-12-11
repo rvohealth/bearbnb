b448613aadcd404c1806f81727877655b3b58cb1
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-12-10 18:56:05 -0600

    Rename generated RoomDen to Den and RoomLivingRoom to LivingRoom
    ```console
    pnpm psy sync
    ```

--- api/src/app/models/Room/Den.ts
+++ api/src/app/models/Room/Den.ts
@@ -1,16 +1,15 @@
-import { Decorators, STI } from '@rvoh/dream'
-import { DreamColumn, DreamSerializers } from '@rvoh/dream/types'
 import Room from '@models/Room.js'
+import { Decorators, STI } from '@rvoh/dream'
+import { DreamSerializers } from '@rvoh/dream/types'
 
-const deco = new Decorators<typeof RoomDen>()
+const deco = new Decorators<typeof Den>()
 
 @STI(Room)
-export default class RoomDen extends Room {
-  public override get serializers(): DreamSerializers<RoomDen> {
+export default class Den extends Room {
+  public override get serializers(): DreamSerializers<Den> {
     return {
       default: 'Room/DenSerializer',
       summary: 'Room/DenSummarySerializer',
     }
   }
-
 }

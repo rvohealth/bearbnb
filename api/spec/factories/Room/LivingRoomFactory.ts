b3b9092a90ebe913484b5e5ceef801ef2c1946fe
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-12-10 19:18:32 -0600

    Rooms controller specs passing
    NOTE: if, after running `pnpm psy sync`, your editor starts showing errors like
    `Unsafe argument of type error typed assigned to a parameter of type 'undefined'.`
    OR
    `Unsafe assignment of an error typed value.`,
    restart the ESLint server (the `ESLint: Restart ESLint Server`
    command in VSCode / Cursor)
    
    Note the changes in api/spec/unit/controllers/V1/Host/Places/RoomsController.spec.ts:
    
    1. The room is no longer the base STI model because, in STI, we can't instantiate
       the base model, only child models, so we choose an arbitrary Room child to stand
       in for any Room.
         • to this end, the `roomFactory` was deleted, and all of the child
           room factories updated with the initialization lines from `roomFactory`
         • similarly, removed the serializer from the base Room model, which necessitated
           removing override from the room STI child models
    2. The request body to the create and update endpoints are changed to reflect the STI
       child we are creating / updating.
    
    ```console
    pnpm psy sync
    pnpm build:spec # to surface any errors so you can fix them
    pnpm uspec spec/unit/controllers/V1/Host/Places/RoomsController.spec.ts
    // The NODE_DEBUG psychic flag can be helpful in debugging controller problems:
    NODE_DEBUG=psychic pnpm uspec spec/unit/controllers/V1/Host/Places/RoomsController.spec.ts
    // There is also an sql flag. They can be applied simultaneously:
    // `NODE_DEBUG=sql,psychic`
    ```

--- api/spec/factories/Room/LivingRoomFactory.ts
+++ api/spec/factories/Room/LivingRoomFactory.ts
@@ -1,8 +1,10 @@
 import LivingRoom from '@models/Room/LivingRoom.js'
 import { UpdateableProperties } from '@rvoh/dream/types'
+import createPlace from '../PlaceFactory.js'
 
 export default async function createRoomLivingRoom(attrs: UpdateableProperties<LivingRoom> = {}) {
   return await LivingRoom.create({
+    place: attrs.place ? null : await createPlace(),
     ...attrs,
   })
 }
